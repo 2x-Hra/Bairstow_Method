7025c7d4dd9cdff8cb816d06e80d8f62c3e2475e
Hossein2x <hrakbari77@gmail.com>
2020-01-17 20:35:15 +0330

    c_cal tested

--- Bairstow2.py
+++ Bairstow2.py
@@ -1,4 +1,5 @@
 
+''' FUNCTIONS '''
 
 def b_cal_(coef_arr, k , n,r_coef,s_coef):
 
@@ -12,7 +13,11 @@ def c_cal_(coef_arr, k , n,r_coef,s_coef):
 	if (k == n+1 or k == n+2):
 		return 0
 
-	return coef_arr[k] + r_coef * b_cal_(coef_arr,k+1,n,r_coef,s_coef) + s_coef * b_cal_(coef_arr,k+2,n,r_coef,s_coef)
+	return coef_arr[k] + r_coef * c_cal_(coef_arr,k+1,n,r_coef,s_coef) + s_coef * c_cal_(coef_arr,k+2,n,r_coef,s_coef)
+
+
+''' Main '''
+
 coef_b = []
 coef_c = []
 coef_a = [6.0000,4.0000,3.0000,1.0000,1.0000]
@@ -24,7 +29,12 @@ while (i<len(coef_a)):
 	coef_b.append(temp)
 	i +=1
 print(coef_b)
-
+i=0
+while (i<len(coef_b)):
+	temp = c_cal_(coef_b,i,len(coef_b)-1,r,s)
+	coef_c.append(temp)
+	i +=1
+print(coef_c)
 # def D_cal_(c1,c2,c3):
 
 # def D1_cal(b0,b1,c2,c3):
