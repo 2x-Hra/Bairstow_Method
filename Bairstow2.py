00c09984c44273b6b10caa3051aaa9facf02b8fd
Hossein2x <hrakbari77@gmail.com>
2020-01-19 17:47:52 +0330

    r and S checked

--- Bairstow2.py
+++ Bairstow2.py
@@ -152,4 +152,12 @@ if __name__ == "__main__":
 	
 	# print(b_array_calculator(list(reversed(coef_a)),coef_b,r,s)) 
 	# print(c_array_calculator(coef_b,coef_c,r,s)) 
+	# s_prev = s
+	# print("S prev :" + str(s_prev))
+	# s_next = s_cal_(s_prev,D2_cal_(coef_b,coef_c),D_cal_(coef_c))
+	# print("S next :" + str(s_next))
+	# r_prev = r
+	# print("r prev :" + str(r_prev))
+	# r_next=r_cal_(r_prev,D1_cal_(coef_b,coef_c),D_cal_(coef_c))
+	# print("r next :" + str(r_next))
 	
