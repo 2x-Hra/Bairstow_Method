c2e6f0de84ad3937044835714bd9cf8b210f349e
Hossein2x <hrakbari77@gmail.com>
2020-01-24 16:50:31 +0330

    Deleted for security reasons

--- Bairstow2.py
+++ Bairstow2.py
@@ -20,64 +20,7 @@ if __name__ == "__main__":
 
 
 	
-	coef_b = []
-	coef_c = []
-
-	
-	coef_a =[]
-
-	
-	counter3 = 0
-	degree = int(input())
-	coefficients = input()
-	coefficients_arr = coefficients.split()
-	
-
-	while(counter3 <len(coefficients_arr)):
-		coef_a.append(int(coefficients_arr[counter3]))
-		counter3 +=1	
-
-	# print(coef_a)
-	roots =[]
-	rs_container =[]
-	i=0
-	r = -2.1
-	s = -1.9
-
-	''' ~~~~ ~~~~'''
-
-	while(True):
-
-		if(len(coef_a) > 3 ): # agar bishtr az daraje 2 bood
-			coef_a = calculate_newCoef(coef_a,coef_b,coef_c,roots,r,s,rs_container)
-		
-		else:
-			
-			roots.append(list(my_numpy.roots(coef_a)))
-			break
-	#---------------------------#
-
-	''' Outputs'''
-
-	final_roots =[]
-	final_roots = final_roots_array_maker(roots)
-	result =[0,0]
-	# print(final_roots)
-	counter = 0
-	while(counter < len(final_roots)):
-		if(final_roots[counter].imag >= 0):
-			result[0] = (round(result[0] + final_roots[counter].real,6))
-			result[1] = result[1] + final_roots[counter].imag
-		counter +=1
-	print(str(result[0])+ " " + str(result[1]))
-
-		
-	
-		
-
-
-
-
+# SECURITY REASONS
 
 
 
